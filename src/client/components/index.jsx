f6dbdd7683bb74a1a43f1e17b1d3bbfd6c8a7720
Adam Wang <adamw3494@gmail.com>
2017-02-10 20:41:13 -0500

    add components

--- src/client/components/index.jsx
+++ src/client/components/index.jsx
@@ -1,1 +1,15 @@
 import React from 'react';
+import { hashHistory } from 'react-router';
+import Playlist from './playlist';
+import Song from './song';
+
+class Index extends component {
+  render() {
+    return (
+      <div>
+        <Playlist />
+        <Song />
+      </div>
+    )
+  }
+}
